b72a9f06f1e890122b8f4dd1ae4c4f60c2e6d821
Sigurður Ari Stefánsson <sigurdur.ari@gmail.com>
2025-02-21 11:39:00 +0000

    First commit, sql schema and basic testinput created

---
+++ sql/insert.sql
@@ -0,0 +1,19 @@
+--Mock test insert data
+INSERT INTO hotels (name, location, rating, ratingCount,
+                    accessibility, pets, refundable, photoURL, checkInTime, checkOutTime)
+            VALUES ('Test1', 'Reykjavík', 8.9, 1000, TRUE, FALSE, FALSE, 'photo1.is', '16:30', '10:00');
+
+INSERT INTO hotels (name, location, rating, ratingCount,
+                    accessibility, pets, refundable, photoURL, checkInTime, checkOutTime)
+            VALUES ('Test2', 'Akureyri', 7.5, 200, TRUE, TRUE, FALSE, 'photo2.is', '15:30', '11:00');
+
+
+INSERT INTO hotelRooms (hotelName, date, roomNum, capacity, pricePerNight)
+            VALUES('Test1', 2024-12-10, 1, 4, 12000);
+INSERT INTO hotelRooms (hotelName, date, roomNum, capacity, pricePerNight)
+            VALUES('Test1', 2024-12-11, 2, 2, 8000);
+INSERT INTO hotelRooms (hotelName, date, roomNum, capacity, pricePerNight)
+            VALUES('Test2', 2024-12-10, 1, 4, 12000);
+INSERT INTO hotelRooms (hotelName, date, roomNum, capacity, pricePerNight)
+            VALUES('Test2', 2024-12-11, 2, 3, 7000);
+
---
+++ sql/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS hotels (
+    name TEXT PRIMARY KEY,
+    location TEXT,
+    rating REAL,
+    ratingCount INTEGER,
+    accessibility INTEGER CHECK (accessibility IN (0,1))
+    pets INTEGER CHECK (pets IN (0,1)),
+    refundable INTEGER CHECK (refundable IN (0,1)),
+    photoURL TEXT,
+    checkInTime TEXT,
+    checkOutTime TEXT
+);
+
+CREATE TABLE IF NOT EXISTS hotelRooms (
+    hotelName TEXT REFERENCES hotels(name),
+    date TEXT CHECK (date LIKE '____-__-__'),
+    roomNum INTEGER,
+    capacity INTEGER,
+    pricePerNight INTEGER,
+    PRIMARY KEY (hotelName, date, roomNum)
+);
+
+CREATE TABLE IF NOT EXISTS booking (
+    username TEXT,
+    roomNumber INTEGER,
+    hotelName TEXT,
+    date TEXT CHECK (date LIKE '____-__-__'),
+    FOREIGN KEY (hotelName, date, roomNumber) REFERENCES hotelRooms(hotelName, date, roomNum)
+);
